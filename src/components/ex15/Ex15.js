25feffb26c89bfae2074f0d47041c1f856b084ce
thefigtree <ldj8431@naver.com>
2023-09-04 21:10:05 +0900

    useForm end

--- src/components/ex15/Ex15.js
+++ src/components/ex15/Ex15.js
@@ -1,10 +1,8 @@
 import { Router } from "./Router";
-// import { Login } from "./pages/Login";
 
 export const Ex15 = () => {
   return (
     <div>
-      {/* <Login></Login> */}
       <Router></Router>
     </div>
   );
